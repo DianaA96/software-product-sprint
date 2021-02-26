ac05147e3e25420e68b308e7ea2ba1244f50bd1c
Diana Aguirre <a01276380@itesm.mx>
2021-02-26 10:16:49 +0000

    Cleaning original document. Adding the header of the portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random fun fact to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomFunFact() {
+  const funFacts =
+      ['I love chocolate', 'I love tacos but hate onion', 'I want to write a book in the future', 'I am afraid of the dark'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random funfact.
+  const funFact = funFacts[Math.floor(Math.random() * funFacts.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const funFactContainer = document.getElementById('funFact-container');
+  funFactContainer.innerText = funFact;
 }
